6efbe37d4051d87a0cd30076c3a2943c2cd01c5d
Seris355 <retyokopka@gmail.com>
2026-05-09 17:16:15 +0300

    feat: register schedule group roster routes

--- server/routes/scheduleRoutes.js
+++ server/routes/scheduleRoutes.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
+const scheduleGroupController = require('../controllers/scheduleGroupController');
 const { verifyToken, requireAdmin } = require('../middleware/authMiddleware');
 
 router.get('/', scheduleController.getAllSchedules);
+
+router.get('/:id/group', verifyToken, requireAdmin, scheduleGroupController.getScheduleGroup);
+
+router.post(
+  '/:id/participants',
+  verifyToken,
+  requireAdmin,
+  scheduleGroupController.addParticipantToSchedule
+);
+
+router.delete(
+  '/:id/participants/:participantId',
+  verifyToken,
+  requireAdmin,
+  scheduleGroupController.removeParticipantFromSchedule
+);
 
 router.get('/:id', scheduleController.getScheduleById);
 
