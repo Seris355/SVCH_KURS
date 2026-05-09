7721288e3568939a85b4c183fc8a71de2123aece
Seris355 <retyokopka@gmail.com>
2026-05-09 17:03:26 +0300

    feat: add scheduleGroupService for admin group operations

---
+++ src/services/scheduleGroupService.js
@@ -0,0 +1,22 @@
+import api from './api';
+
+export const scheduleGroupService = {
+  getGroup: async (scheduleId) => {
+    const response = await api.get(`/schedules/${scheduleId}/group`);
+    return response.data;
+  },
+
+  addParticipant: async (scheduleId, participantId) => {
+    const response = await api.post(`/schedules/${scheduleId}/participants`, {
+      participantId,
+    });
+    return response.data;
+  },
+
+  removeParticipant: async (scheduleId, participantId) => {
+    const response = await api.delete(
+      `/schedules/${scheduleId}/participants/${participantId}`
+    );
+    return response.data;
+  },
+};
