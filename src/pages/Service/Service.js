c769654fc0e89ff063a64165ba36a42426e14f67
Seris355 <retyokopka@gmail.com>
2026-05-21 08:51:49 +0300

    refactor: remove unused React imports from Service page

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer.js';
 import './service_style.css';
