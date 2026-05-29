238bd487d1160001bcea04f8c60b13f52689f6b5
Seris355 <retyokopka@gmail.com>
2026-05-29 15:48:55 +0300

    feat: init all database tables via models index

--- server/scripts/initDB.js
+++ server/scripts/initDB.js
@@ -1,13 +1,37 @@
-const { sequelize, Instructor, Participant, MasterClass, ParticipantPassword, RefreshToken } = require('../models');
+const { sequelize } = require('../models');
+
+const TABLE_NAMES = [
+  'instructors',
+  'participants',
+  'participant_passwords',
+  'masterclasses',
+  'master_class_participants',
+  'categories',
+  'master_class_categories',
+  'locations',
+  'schedules',
+  'payments',
+  'reviews',
+  'favorites',
+  'refresh_tokens',
+  'contact_requests',
+  'contact_threads',
+  'contact_messages',
+  'tests',
+  'questions',
+  'answers',
+  'test_results',
+];
 
 async function initDatabase() {
   try {
-    
     await sequelize.authenticate();
     console.log('Подключение к базе данных установлено.');
 
     await sequelize.sync({ alter: true });
     console.log('Модели синхронизированы с базой данных.');
+    console.log('Созданы/обновлены таблицы:');
+    TABLE_NAMES.forEach((name) => console.log(`  - ${name}`));
 
     console.log('База данных успешно инициализирована!');
     process.exit(0);
@@ -18,4 +42,3 @@ async function initDatabase() {
 }
 
 initDatabase();
-
