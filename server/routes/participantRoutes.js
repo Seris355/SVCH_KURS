d2f33deff5377fe51576cb862c66a14736a1587a
Seris355 <retyokopka@gmail.com>
2026-06-04 10:07:07 +0300

    fix: partipiciants routes post, put

--- server/routes/participantRoutes.js
+++ server/routes/participantRoutes.js
@@ -7,6 +7,8 @@ const { verifyToken, requireAdmin } = require('../middleware/authMiddleware');
 router.get('/', verifyToken, requireAdmin, participantController.getAllParticipants);
 router.get('/export/pdf', verifyToken, requireAdmin, participantController.exportParticipantsPdf);
 router.get('/:id/exists', verifyToken, requireAdmin, participantController.checkParticipantExists);
+router.post('/', verifyToken, requireAdmin, participantController.createParticipant);
+router.put('/:id', verifyToken, requireAdmin, participantController.updateParticipant);
 router.get('/:id', verifyToken, requireAdmin, participantController.getParticipantById);
 router.put('/:id/password', verifyToken, requireAdmin, participantController.changePassword);
 router.delete('/:id', verifyToken, requireAdmin, participantController.deleteParticipant);
